f5a3c01b42ca74bdb5624f05980b3557d193275f
Josué Girón <josue.giron@mnc.gt>
2019-07-31 17:50:44 -0600

    Ejemplo 2.3

--- bienvenido.c
+++ bienvenido.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-/* Inicio del main para aprender */
+/* main example*/
 int main(){
-    printf("Hola Mundo!\n");
+
+    int entero1, entero2, total;
+
+    printf("Introduzca el primer número: \n");
+    scanf("%d", &entero1);
+
+    printf("Introduzca el segundo número: \n");
+    scanf("%d", &entero2);
+    
+    total = entero1 + entero2;
+
+    printf("El total es: %d\n", total);
+
     return 0; 
-} /* Fin del main */
+} /* main */
