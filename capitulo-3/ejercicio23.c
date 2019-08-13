7210da5ad7cd91f6eb9a98fd8dc87f6b86f64d10
Josué Girón <josue.giron@mnc.gt>
2019-08-12 18:10:58 -0600

    Agregar ejercicio 23

---
+++ capitulo-3/ejercicio23.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* main */
+int main(){
+
+    int i = 0;
+
+    while( i < 10){
+
+        printf("%d\t", ++i);
+
+    }/* end while */
+
+    printf("\n");
+    
+}/* end main */
