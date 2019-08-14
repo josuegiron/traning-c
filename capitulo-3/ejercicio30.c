72d4d1e931019fb675b76de8b4bc99918de1b916
Josué Girón <josue.giron@mnc.gt>
2019-08-13 18:38:34 -0600

    Agregar ejercicio 30

---
+++ capitulo-3/ejercicio30.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    
+    int fila = 10;
+    int columna;
+
+    while ( fila >= 1 ){
+
+        columna = 1;
+
+        while ( columna <= 10 ){
+
+            printf("%s", (fila % 2) ? "<": ">");
+            columna++;
+
+        }
+
+        fila--;
+        printf("\n");
+        
+    }
+
+    return 0;
+
+}
