83bc38ba3df187b12b3703bca855b37f272cb76d
Josué Girón <josue.giron@mnc.gt>
2019-08-14 19:16:56 -0600

    Agregar palindromo

---
+++ capitulo-3/ejercicio35.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/* main */
+int main(){
+
+    int numero;
+    int n = 0;
+    int palindromo = 1;
+
+    printf("Ingrese un número para validar si es palindromo: ");
+    scanf("%d", &numero);
+
+    int val = numero;
+
+    while ( val != 0 ){
+
+        val = val / 10;
+        n++;
+        
+    }/* end while */
+
+    
+    
+    while( n > 0 ){
+        int x = 10, j = n;
+
+        while (j != 1){
+            j--;
+            x *= 10;
+            if (n <= 1){
+                x = 1;
+            }
+        }
+        
+
+        int primerDigito = numero / x;
+        
+        int ultimoDigito = numero % 10;
+
+        
+        numero = numero % x;
+        numero = numero / 10;
+        printf("%d\t%d\t%d\t%d\t%d\n", primerDigito, ultimoDigito, n, numero, x);
+
+        if ( primerDigito != ultimoDigito ){
+            
+            palindromo = 0;    
+
+        }/* end if */
+        n -= 2;
+
+        
+
+    }/* end while */
+
+    if ( palindromo == 1 ){
+        
+        printf("Es un palindromo!\n");
+
+    }else{  
+
+        printf("No es un palindromo!\n");
+        
+    }// no es un palindromo
+
+}/* end main */
