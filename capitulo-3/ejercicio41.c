ae15ce6b6a400b605623f8a323c984515c6e4dca
Josué Girón <josue.giron@mnc.gt>
2019-08-16 17:53:47 -0600

    Agregar ejercicio 41

---
+++ capitulo-3/ejercicio41.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// main
+int main(){
+
+    int multiplo = 1;
+
+    while (1){
+
+        multiplo *= 2;
+        printf("%d\n", multiplo);
+        
+    }// end while
+
+}// end main
+
+// R: Imprime cero infinitamente.
